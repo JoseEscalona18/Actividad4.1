0d05e3e09649901946af2ee14e1a2913137a4eee
Jupagolab <114892574+Jupagolab@users.noreply.github.com>
2023-07-07 13:32:15 -0400

    Update app.js
    Se corregieron algunos errores, y se agrego la busqueda de sinonimos y texto de fonetica

--- app.js
+++ app.js
@@ -1,72 +1,85 @@
 const API = "https://api.dictionaryapi.dev/api/v2/entries/en/"
  
-
-const inputpalabra = document.getElementById('palabra')
+const inputPalabra = document.getElementById('palabra')
 const boton = document.getElementById('boton')
 const resultado = document.getElementById('resultado')
-const Darkmode = document.getElementById('Darkmode');
+const darkMode = document.getElementById('Darkmode');
 const fuentes = document.getElementById('Fuentes');
 const body = document.body
 
-function Busquedadebounce(func, delay){
-    let timeoutId;
-    return function(...args){
-        if (timeoutId) {
-            clearTimeout(timeoutId)
-        }
-        timeoutId = setTimeout(()=>{
-            func.apply(this, args);
-        }, delay);
+function busquedaDebounce(func, delay){
+  let timeoutId;
+  return function(...args){
+    if (timeoutId) {
+      clearTimeout(timeoutId)
     }
+    timeoutId = setTimeout(()=>{
+      func.apply(this, args);
+    }, delay);
+  }
 }
 
-const buscar = Busquedadebounce(()=> {
+const buscar = busquedaDebounce(()=> {
+  const Palabra = inputPalabra.value;
+  fetch(`${API}${Palabra}`) 
+    .then(res => res.json())
+    .then(response => {
+      const data = response[0];
+      const definitions = data.meanings.map(meaning => meaning.definitions[0].definition);
+      const sinonimos = data.meanings.flatMap(meaning => meaning.synonyms);
+      const fonetica = data.phonetics.filter(texto => texto.text);
 
-    const Palabra = inputpalabra.value
+      //se agregan las foneticas a una variable string
+      let textoFonetica = '';
 
-        fetch(API+Palabra) 
-        .then(res => res.json())
-        .then(response => {
-            const data = response[0]
-            const definitions = data.meanings.map(meaning => meaning.definitions[0].definition);
-            resultado.innerHTML = `
-            <h2> ${data.word}</h2>
-            <ul>
-            ${definitions.map(definition => `<li>${definition}</li>`).join('')}
-            </ul>`
-        
+      if (fonetica.length == 0){
+        textoFonetica='No hay fonetica disponible en la API'
+      }else {
+        for (i = 0; i < fonetica.length; i++){
+          if (i==1){
+            textoFonetica=`${textoFonetica}<br> <b>Tambien:</b> `
+          }
+          textoFonetica = textoFonetica + ' ' + fonetica[i].text;
+        }
+      }
 
-        })
-        .catch(error => {resultado.innerHTML = `No se encontraron resultados para ${Palabra}`});
+      //agregar informacion al html
+      resultado.innerHTML = `
+      <h2> ${data.word}</h2>
+      <ul>
+      ${definitions.map(definition => `<li>${definition}</li>`).join('')}
+      <h3>Sinónimos:</h3>
+      ${sinonimos.length 
+        ? `<ul>${sinonimos.map(sinonimo => `<li>${sinonimo}</li>`).join('')}</ul>`
+        : '<p>No hay sinónimos disponibles.</p>'
+      }
+      <h3>Fonética:</h3>
+      <p>${textoFonetica}</p>
+      </ul>`
+  
+    })
+    .catch(error => {resultado.innerHTML = `No se encontraron resultados para ${Palabra}`});
     
 }, 500);
 
-inputpalabra.addEventListener('input', buscar)
+inputPalabra.addEventListener('input', buscar)
 boton.addEventListener('click', buscar)
 
 fuentes.addEventListener('change', ()=> {
-    const fuentesclass = fuentes.value
+  const fuentesclass = fuentes.value
 
-    if(body.classList.contains('Darkmode')){
-        body.classList = ''
-        body.classList.add('Darkmode', fuentesclass)
-    } else {
-        body.classList = ''
-        body.classList.add(fuentesclass) 
-    }
-})
+  if(body.classList.contains('Darkmode')){
+    body.classList = ''
+    body.classList.add('Darkmode', fuentesclass)
+  } else {
+    body.classList = ''
+    body.classList.add(fuentesclass) 
+  }
+});
 
-
-Darkmode.addEventListener('click',()=> {
-    body.classList.toggle('Darkmode');
-    if (body.classList.contains('Darkmode')){
-        Darkmode.textContent = 'Modo claro'
-    } else Darkmode.textContent = 'Modo Oscuro'
-})
-
-
-
-
-
-
-
+darkMode.addEventListener('click',()=> {
+  body.classList.toggle('Darkmode');
+  if (body.classList.contains('Darkmode')){
+    darkMode.textContent = 'Modo claro'
+  } else darkMode.textContent = 'Modo Oscuro'
+});
